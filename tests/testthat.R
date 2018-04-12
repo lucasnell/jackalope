454b75c710e9bfa95f6cef18b6b50702db96cd37
Lucas Nell <lucas@lucasnell.com>
2018-04-12 14:58:15 -0500

    Added tests for mutation accuracy

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(gemino)
+
+test_check("gemino")
---
+++ tests/testthat/test_mutations.R
@@ -0,0 +1,65 @@
+context("Testing mutation accuracy")
+
+# Simulating mutations in R and gemino's C++ code and making sure produce
+# the same output
+
+library(gemino)
+
+n_seqs <- 10
+n_vars <- 10
+n_muts <- 500
+len <- 100
+
+
+seqs <- rando_seqs(n_seqs, len)
+vars <- gemino:::make_vars(seqs, n_vars)
+vars_R <- replicate(n_vars, seqs, simplify = FALSE)
+
+
+for (v in 0:(n_vars-1)) {
+    ts <- vars_R[[(v+1)]]
+    for (s in 0:(length(seqs)-1)) {
+        m = 0;
+        max_size = gemino:::see_sizes(vars, v)[(s+1)]
+        while (m < n_muts && max_size > 0) {
+            pos = as.integer(runif(1) * max_size)
+            rnd = runif(1);
+            if (rnd < 0.5) {
+                str = gemino:::rando_seqs(1, 1)
+                gemino:::add_substitution(vars, v = v, scaff = s,
+                                          nucleo = str, new_pos_ = pos)
+                substr(ts[(s+1)], pos+1, pos+1) <- str
+            } else if (rnd < 0.75) {
+                size = as.integer(rexp(1, 2.0) + 1.0)
+                if (size > 10) size = 10
+                str = gemino:::rando_seqs(1, size + 1)
+                gemino:::add_insertion(vars, v = v, scaff = s,
+                                       nucleos_ = str, new_pos_ = pos)
+                ts[(s+1)] <- paste0(substr(ts[(s+1)], 1, pos), str,
+                                    substr(ts[(s+1)], pos + 2, nchar(ts[(s+1)])))
+                max_size <- max_size + size
+            } else {
+                size = as.integer(rexp(1, 2.0) + 1.0)
+                if (size > 10) size = 10
+                if (size > (max_size - pos)) size = max_size - pos;
+                gemino:::add_deletion(vars, v = v, scaff = s,
+                                      size_ = size, new_pos_ = pos)
+                ts[(s+1)] <- paste0(substr(ts[(s+1)], 1, pos),
+                                    substr(ts[(s+1)], pos + 1 + size,
+                                           nchar(ts[(s+1)])))
+                max_size = max_size - size
+            }
+            m <- m + 1
+        }
+    }
+    vars_R[[(v+1)]] <- ts
+}
+
+# Converting to list like vars_R:
+vars_cpp <- lapply(1:n_vars, function(i) gemino:::see_vg(vars, i-1))
+
+
+
+test_that("Mutations produced are accurate", {
+    expect_identical(vars_R, vars_cpp)
+})
