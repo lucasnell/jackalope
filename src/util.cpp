8d88e29599b270e4ed24cc31ec577ad5ad972428
Lucas Nell <lucas@lucasnell.com>
2018-04-15 15:26:49 -0500

    Moved simple util.* functions to header
    Including a template to avoid linker error

--- src/util.cpp
+++ src/util.cpp
@@ -12,28 +12,8 @@ using namespace Rcpp;
 
 
 
-/*
- Clear memory from a std::vector, std::deque, or std::string.
- Simply erasing objects does not clear memory.
- I'm not sure this works for other classes. You should check before using this function.
- */
-template <class U>
-void clear_memory(U& x) {
-    U(x.begin(), x.end()).swap(x);
-}
 
 
-
-/*
- Get a size from either an arma::uvec or std::vector<uint>.
- This is used in template functions that work for either class.
- */
-inline uint uints_get_size(std::vector<uint>& uints) {
-    return uints.size();
-}
-inline uint uints_get_size(arma::uvec& uints) {
-    return uints.n_elem;
-}
 
 
 
--- src/util.h
+++ src/util.h
@@ -22,11 +22,28 @@
 using namespace Rcpp;
 
 
-template <class U>
-void clear_memory(U& x);
+/*
+ Clear memory from a std::vector, std::deque, or std::string.
+ Simply erasing objects does not clear memory.
+ I'm not sure this works for other classes. You should check before using this function.
+ */
+template <typename U>
+void clear_memory(U& x) {
+    U(x.begin(), x.end()).swap(x);
+}
 
-inline uint uints_get_size(std::vector<uint>& uints);
-inline uint uints_get_size(arma::uvec& uints);
+
+/*
+ Get a size from either an arma::uvec or std::vector<uint>.
+ This is used in template functions that work for either class.
+ */
+inline uint uints_get_size(std::vector<uint>& uints) {
+    return uints.size();
+}
+inline uint uints_get_size(arma::uvec& uints) {
+    return uints.n_elem;
+}
+
 
 double gc_prop(const std::string& sequence);
 double gc_prop(const std::string& sequence, const uint& start, const uint& stop);
