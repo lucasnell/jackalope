7946fc02ee8802e7537c637592e7809cb3c63223
Lucas Nell <lucas@lucasnell.com>
2018-06-21 17:38:29 -0500

    Began making recombination version
    It does NOT work yet [skip ci]

--- src/mevo_phylo.cpp
+++ src/mevo_phylo.cpp
@@ -181,6 +181,161 @@ inline int one_tree_no_recomb_(VarSet& vars,
 
 
 
+
+/*
+ Same thing as above, but with recombination, which means that mutations must
+ occur inside a range that changes with indels.
+ Only the ending position will change, so the starting position is kept constant.
+ Look for lines ending in `// ***` for those that differ from the non-recombination
+ version.
+
+ Look for `// <<<<<<<<<<` for stuff that needs to be updated for the
+ recombination version.
+ */
+template <typename T>
+inline int one_tree_recomb_(VarSet& vars,
+                            const uint32& start,
+                            uint32& end,
+                            const T& sampler_base,
+                            const uint32& seq_ind,
+                            const std::vector<double>& branch_lens,
+                            const arma::Mat<uint32>& edges,
+                            const std::vector<uint32>& spp_order,
+                            const arma::mat& gamma_mat,
+                            pcg32& eng,
+                            Progress& progress,
+                            const std::vector<uint32>& progress_branch_lens,
+                            std::vector<uint32>& n_muts) {
+
+
+    // # tips = # variants
+    uint32 n_tips = vars.size();
+    // tree size is # tips plus # nodes
+    uint32 tree_size = edges.max() + 1;
+    // Number of edges = the number of connections between nodes/tips
+    uint32 n_edges = edges.n_rows;
+
+    /*
+     Create tree of empty VarSequence objects
+     */
+    std::vector<VarSequence> var_seqs(tree_size,
+                                      VarSequence(vars.reference[seq_ind]));// <<<<<<<<<<
+
+    /*
+     Create corresponding tree of MutationSampler objects
+     First fill with the base sampler, then fill in pointers to the corresponding
+     VarSequence object, then fill in the gamma matrix.
+     */
+    std::vector<T> samplers(tree_size, sampler_base);
+    for (uint32 i = 0; i < tree_size; i++) {
+        samplers[i].fill_ptrs(var_seqs[i]);
+        samplers[i].fill_gamma(gamma_mat);
+    }
+
+
+    /*
+     Exponential distribution to do the time-jumps along the branch lengths:
+     */
+    std::exponential_distribution<double> distr;
+
+    /*
+     Set up vector of overall sequence rates.
+     They should all be the same as the first one to start out.
+     */
+    std::vector<double> seq_rates(tree_size, samplers[0].total_rate(start, end));  // ***
+
+    /*
+     Now iterate through the phylogeny:
+     */
+    for (uint32 i = 0; i < n_edges; i++) {
+
+        if (progress.is_aborted()) return -1;
+
+        uint32 n_muts_ = 0;
+
+        // Indices for nodes/tips that the branch length in `branch_lens` refers to
+        uint32 b1 = edges(i,0);
+        uint32 b2 = edges(i,1);
+        /*
+         Replace existing mutation information in VarSequence at `b1` with info in the
+         one at `b2`
+         */
+        // THIS NEEDS TO BE FIXED:
+        samplers[b2].vs->replace(*samplers[b1].vs);  // <<<<<<<<<<
+        /*
+         Do the same for the SeqGammas in the sampler:
+         */
+        // SO DOES THIS:
+        samplers[b2].location.mr().gammas =
+            samplers[b1].location.mr().gammas;// <<<<<<<<<<
+        /*
+         Update overall sequence rate:
+         */
+        // SO DOES THIS:
+        double& rate(seq_rates[b2]);  // <<<<<<<<<<
+        rate = seq_rates[b1];  // <<<<<<<<<<
+
+        // Set exponential distribution to use this sequence's rate:
+        distr.param(std::exponential_distribution<double>::param_type(rate));
+
+        /*
+         Now do exponential jumps and mutate until you exceed the branch length.
+         */
+        double amt_time = branch_lens[i];
+        double time_jumped = distr(eng);
+        double rate_change = 0;
+        while (time_jumped <= amt_time && end >= start) {  // ***
+            /*
+             Add mutation here, outputting how much the overall sequence rate should
+             change:
+             */
+            rate_change = samplers[b2].mutate_rate_change(eng, start, end);  // ***
+            n_muts_++;
+            /*
+             Adjust the overall sequence rate, then update the exponential distribution:
+             */
+            rate += rate_change;
+            distr.param(std::exponential_distribution<double>::param_type(rate));
+            /*
+             Jump again:
+             */
+            time_jumped += distr(eng);
+        }
+        /*
+         Clear info from VarSequence object at `b1` if it's no longer needed to
+         free up some memory.
+         (If it's the last branch length, `b1` will always be a node and thus no longer
+         needed.)
+         */
+        bool clear_b1;
+        if (i < (n_edges - 1)) {
+            // Is it absent from any remaining items in the first column?
+            clear_b1 = ! arma::any(edges(arma::span(i+1, edges.n_rows - 1), 0) == b1);
+        } else clear_b1 = true;
+        if (clear_b1) samplers[b1].vs->clear();
+
+        progress.increment(progress_branch_lens[i]);
+
+        n_muts[i] = n_muts_;
+    }
+
+    /*
+     Update final `VarSequence` objects using the index vector `spp_order`:
+     */
+    for (uint32 i = 0; i < n_tips; i++) {
+        uint32 j = spp_order[i];
+        vars[i][seq_ind].replace(var_seqs[j]);  // <<<<<<<<<<
+    }
+
+    return 0;
+
+}
+
+
+
+
+
+
 /*
  This function produces `spp_order` in `test_phylo`.
  This object is a vector of which indices in the phylogeny tips should go first.
@@ -286,6 +441,9 @@ std::vector<uint32> test_phylo(SEXP& vs_sexp,
 
     return n_muts;
 }
+
+
+
 
 
 
