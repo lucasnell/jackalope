523235f71d709240b14426d7c988772aacadc9d9
Lucas Nell <lucas@lucasnell.com>
2018-07-01 23:18:24 -0500

    Started methods to access major C++ classes from R
    [skip ci]

--- src/sequence_classes.cpp
+++ src/sequence_classes.cpp
@@ -262,8 +262,86 @@ char VarSequence::get_char_(const uint32& new_pos,
 
 
 
+/*
+ ========================================================================================
+ ========================================================================================
+
+ These functions are to access fields from R
+
+ ========================================================================================
+ ========================================================================================
+ */
 
 
+/*
+ ----------------------------------------
+ Reference genome methods
+ ----------------------------------------
+ */
+
+
+//[[Rcpp::export]]
+std::vector<uint32> see_ref_genome_seq_sizes(SEXP ref_genome_) {
+    XPtr<RefGenome> ref_genome(ref_genome_);
+    std::vector<uint32> out = ref_genome->seq_sizes();
+    return out;
+}
+// Check for no duplicates
+//[[Rcpp::export]]
+void remove_ref_genome_seq_sizes(
+        SEXP ref_genome_,
+        std::vector<uint32> seq_inds) {
+
+    XPtr<RefGenome> ref_genome(ref_genome_);
+    std::deque<RefSequence>& sequences(ref_genome->sequences);
+
+    std::sort(seq_inds.begin(), seq_inds.end());
+
+    for (uint32 i = 0; i < seq_inds.size(); i++) {
+        uint32 j = seq_inds[i];
+        sequences.erase(sequences.begin() + j);
+        for (uint32 k = i+1; k < seq_inds.size(); k++) seq_inds[k]--;
+    }
+    clear_memory<std::deque<RefSequence>>(sequences);
+    return;
+}
+
+
+//[[Rcpp::export]]
+std::vector<std::string> see_ref_genome_seq_names(SEXP ref_genome_) {
+    XPtr<RefGenome> ref_genome(ref_genome_);
+    std::vector<std::string> out;
+    out.reserve(ref_genome->size());
+    for (const RefSequence& seq : (*ref_genome).sequences) out.push_back(seq.name);
+    return out;
+}
+// Check for seq_inds and names lengths being equal
+//[[Rcpp::export]]
+void set_ref_genome_seq_names(
+        SEXP ref_genome_,
+        const std::vector<uint32>& seq_inds,
+        const std::vector<std::string>& names) {
+    XPtr<RefGenome> ref_genome(ref_genome_);
+    for (uint32 i = 0; i < seq_inds.size(); i++) {
+        (*ref_genome)[seq_inds[i]].name = names[i];
+    }
+    return;
+}
+
+//[[Rcpp::export]]
+std::string see_ref_genome_seq(SEXP ref_genome_, const uint32& seq_ind) {
+    XPtr<RefGenome> ref_genome(ref_genome_);
+    std::string out = (*ref_genome)[seq_ind].nucleos;
+    return out;
+}
+
+
+
+/*
+ ----------------------------------------
+ Variants methods
+ ----------------------------------------
+ */
 
 
 
