c5cf1bf1a69eebc6314fa6ee8664970b53bd4c5e
Lucas Nell <lucas@lucasnell.com>
2019-03-06 11:56:18 -0600

    Cleaned description, removed unnecessary copyright info

--- src/sequencer.h
+++ src/sequencer.h
@@ -2,44 +2,6 @@
 #define __GEMINO_SEQUENCER_H
 
 
-/*
- ==========================================================================
-                         Mason - A Read Simulator
- ==========================================================================
- Copyright (c) 2006-2016, Knut Reinert, FU Berlin
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of Knut Reinert or the FU Berlin nor the names of
-       its contributors may be used to endorse or promote products derived
-       from this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- DAMAGE.
-
- ==========================================================================
- Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
- ==========================================================================
-
- Edited for use in gemino by Lucas Nell, 2018
-
- */
 
 #include <RcppArmadillo.h>
 #include <algorithm> // lower_bound
@@ -502,6 +464,9 @@ public:
         std::vector<std::vector<uint8>> err_codes;
         std::vector<std::vector<sint32>> err_lengths;
         std::vector<uint32> read_seq_spaces;
+        /*
+         `read_seq_spaces` above is the "sequence space": read length + sum(indel lengths)
+         */
 
         /*
         Fill all objects but `reads` and `quals` (those have vector-lengths set, but
