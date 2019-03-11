3d7775e5da6cbbf738163c65d6c8a3de15bea839
Lucas Nell <lucas@lucasnell.com>
2019-03-11 18:56:04 -0500

    Added internal `using_openmp` function

---
+++ src/util.cpp
@@ -0,0 +1,19 @@
+#include <RcppArmadillo.h>
+#include <vector>
+#include <string>
+
+#include "gemino_types.h"  // integer types
+
+
+using namespace Rcpp;
+
+//[[Rcpp::export]]
+bool using_openmp() {
+    bool out = false;
+#ifdef _OPENMP
+    out = true;
+#endif
+    return out;
+}
+
+
