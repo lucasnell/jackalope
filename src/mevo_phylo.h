3a3aecbe122192084ed6ac314462e6409afb11e9
Lucas Nell <lucas@lucasnell.com>
2019-03-12 12:40:41 -0500

    Moved return statement from within OpenMP block in mevo_phylo

--- src/mevo_phylo.h
+++ src/mevo_phylo.h
@@ -639,114 +639,13 @@ public:
             SEXP& ref_genome_ptr,
             SEXP& sampler_base_ptr,
             const std::vector<arma::mat>& gamma_mats,
-            const uint32& n_cores,
+            uint32 n_cores,
             const bool& show_progress);
 
 
 
 };
 
-
-
-/*
- `T` should be `MutationSampler` or `ChunkMutationSampler`.
- */
-template <typename T>
-XPtr<VarSet> PhyloInfo<T>::evolve_seqs(
-        SEXP& ref_genome_ptr,
-        SEXP& sampler_base_ptr,
-        const std::vector<arma::mat>& gamma_mats,
-        const uint32& n_cores,
-        const bool& show_progress) {
-
-    XPtr<RefGenome> ref_genome(ref_genome_ptr);
-    XPtr<T> sampler_base(sampler_base_ptr);
-
-    // Extract tip labels from the first tree:
-    const PhyloTree& first_tree(phylo_one_seqs[0].trees[0]);
-    std::vector<std::string> var_names = first_tree.tip_labels;
-
-    XPtr<VarSet> var_set(new VarSet(*ref_genome, var_names), true);
-
-    uint32 n_seqs = var_set->reference->size();
-    uint64 total_seq = ref_genome->total_size;
-
-    Progress prog_bar(total_seq, show_progress);
-
-    if (n_seqs != gamma_mats.size()) {
-        std::string err_msg = "\ngamma_mats must be of same length as # sequences in ";
-        err_msg += "reference";
-        throw(Rcpp::exception(err_msg.c_str(), false));
-    }
-    if (n_seqs != phylo_one_seqs.size()) {
-        std::string err_msg = "\n# tips in phylo. info must be of same length as ";
-        err_msg += "# sequences in reference genome";
-        throw(Rcpp::exception(err_msg.c_str(), false));
-    }
-
-
-    // Generate seeds for random number generators (1 RNG per core)
-    const std::vector<std::vector<uint64>> seeds = mc_seeds(n_cores);
-
-    #ifdef _OPENMP
-    #pragma omp parallel default(shared) num_threads(n_cores) if (n_cores > 1)
-    {
-    #endif
-
-    std::vector<uint64> active_seeds;
-
-    // Write the active seed per core or just write one of the seeds.
-    #ifdef _OPENMP
-    uint32 active_thread = omp_get_thread_num();
-    active_seeds = seeds[active_thread];
-    #else
-    active_seeds = seeds[0];
-    #endif
-
-    pcg64 eng = seeded_pcg(active_seeds);
-
-    // Parallelize the Loop
-    #ifdef _OPENMP
-    #pragma omp for schedule(static)
-    #endif
-    for (uint32 i = 0; i < n_seqs; i++) {
-
-        PhyloOneSeq<T>& seq_phylo(phylo_one_seqs[i]);
-
-        const arma::mat& gamma_mat(gamma_mats[i]);
-
-        if (gamma_mat(gamma_mat.n_rows-1,0) != (*var_set)[0][i].size()) {
-            std::string err_msg = "\nGamma matrices must have max values equal to ";
-            err_msg += "the respective sequence's length.\n";
-            err_msg += "This error occurred on Gamma matrix number ";
-            err_msg += std::to_string(i+1);
-            throw(Rcpp::exception(err_msg.c_str(), false));
-        }
-
-        // Set values for variant info and sampler:
-        seq_phylo.set_samp_var_info(*var_set, *sampler_base, i, gamma_mat);
-
-        // Evolve the sequence using the seq_phylo object:
-        int code = seq_phylo.evolve(eng, prog_bar);
-
-        // If the code is -1, the user interrupted the process.
-        // Make sure this check happens outside of multithreaded code.
-        if (code == -1) {
-            std::string warn_msg = "\nThe user interrupted phylogenetic evolution. ";
-            warn_msg += "Note that changes occur in place, so your variants have ";
-            warn_msg += "already been partially added.";
-            Rcpp::warning(warn_msg.c_str());
-            return var_set;
-        }
-    }
-
-    #ifdef _OPENMP
-    }
-    #endif
-
-    return var_set;
-
-}
 
 
 
