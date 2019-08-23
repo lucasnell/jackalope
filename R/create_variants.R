1681e9375da42ebdce6e8edc2f56098374bb9273
Lucas Nell <lucas@lucasnell.com>
2019-08-23 16:25:05 -0500

    create_variants: added epsilon arg, fixed R6-class checks

--- R/create_variants.R
+++ R/create_variants.R
@@ -17,22 +17,32 @@
 #'     If this argument is `NULL`, all arguments other than `reference` are ignored,
 #'     and an empty `variants` object with no variants is returned.
 #'     This is designed for use when you'd like to add mutations manually.
-#'     If you create a blank `variants` object, you can= use its `add_vars` method
+#'     If you create a blank `variants` object, you can use its `add_vars` method
 #'     to add variants manually.
 #' @param sub Output from one of the \code{\link{sub_models}} functions that organizes
 #'     information for the substitution models.
 #'     See \code{\link{sub_models}} for more information on these models and
 #'     their required parameters.
 #'     This argument is ignored if you are using a VCF file to create variants.
+#'     Passing `NULL` to this argument results in no substitutions.
 #'     Defaults to `NULL`.
 #' @param ins Output from the \code{\link{indels}} function that specifies rates
 #'     of insertions by length.
+#'     This argument is ignored if you are using a VCF file to create variants.
 #'     Passing `NULL` to this argument results in no insertions.
 #'     Defaults to `NULL`.
 #' @param del Output from the \code{\link{indels}} function that specifies rates
 #'     of deletions by length.
+#'     This argument is ignored if you are using a VCF file to create variants.
 #'     Passing `NULL` to this argument results in no deletions.
 #'     Defaults to `NULL`.
+#' @param epsilon Error control parameter for the "tau-leaping" approximation to
+#'     the Doob–Gillespie algorithm, as used for the indel portion of the simulations.
+#'     Smaller values result in a closer approximation.
+#'     Larger values are less exact but faster.
+#'     Values must be `> 0` and `< 1`.
+#'     For more information, see the references below.
+#'     Defaults to `0.03`.
 #' @param n_threads Number of threads to use for parallel processing.
 #'     This argument is ignored if OpenMP is not enabled.
 #'     Threads are spread across chromosomes, so it
@@ -43,6 +53,24 @@
 #'
 #'
 #' @export
+#'
+#'
+#' @references
+#' Doob, J. L. 1942. Topics in the theory of markoff chains.
+#' \emph{Transactions of the American Mathematical Society} \strong{52}(1): 37–64.
+#'
+#' Gillespie, D. T. 1976. A general method for numerically simulating the stochastic time
+#' evolution of coupled chemical reactions. \emph{Journal of Computational Physics}
+#' \strong{22}(4): 403–434.
+#'
+#' Wieder, N., R. H. Fink, and F. von Wegner. 2011. Exact and approximate stochastic
+#' simulation of intracellular calcium dynamics.
+#' \emph{Journal of Biomedicine and Biotechnology} \strong{2011}: 572492.
+#'
+#' Cao, Y., D. T. Gillespie, and L. R. Petzold. 2006. Efficient step size
+#' selection for the tau-leaping simulation method.
+#' \emph{The Journal of Chemical Physics} \strong{124}(4): 044109.
+#'
 #'
 #'
 #' @examples
@@ -56,6 +84,7 @@ create_variants <- function(reference,
                             sub = NULL,
                             ins = NULL,
                             del = NULL,
+                            epsilon = 0.03,
                             n_threads = 1,
                             show_progress = FALSE) {
 
@@ -102,14 +131,17 @@ create_variants <- function(reference,
     # Do checks and organize molecular-evolution info
     # (or `NULL` if `sub` was not provided):
     # -----------------*
-    if (!is.null(sub) && !is_type(sub, "sub_model_info")) {
-        err_msg("create_variants", "sub", "NULL or a \"sub_model_info\" object")
+    if (!is.null(sub) && !is_type(sub, "sub_info")) {
+        err_msg("create_variants", "sub", "NULL or a \"sub_info\" object")
     }
-    if (!is.null(ins) && !is_type(ins, "indel_rates")) {
-        err_msg("create_variants", "ins", "NULL or a \"indel_rates\" object")
+    if (!is.null(ins) && !is_type(ins, "indel_info")) {
+        err_msg("create_variants", "ins", "NULL or a \"indel_info\" object")
     }
-    if (!is.null(del) && !is_type(del, "indel_rates")) {
-        err_msg("create_variants", "del", "NULL or a \"indel_rates\" object")
+    if (!is.null(del) && !is_type(del, "indel_info")) {
+        err_msg("create_variants", "del", "NULL or a \"indel_info\" object")
+    }
+    if (!single_number(epsilon) || epsilon <= 0 || epsilon >= 1) {
+        err_msg("create_variants", "epsilon", "a single number > 0 and < 1")
     }
     # If sub is NULL and it's not a vcf file method, convert sub to rate-0 matrix:
     if (is.null(sub) && !vcf) sub <- sub_JC69(0)
