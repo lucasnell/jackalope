cc125b6e4567a3206d89d621d5a2a366c77d39a2
Lucas Nell <lucas@lucasnell.com>
2019-08-23 13:44:20 -0500

    MutationSampler uses indel and sub classes, not type and location

--- src/mutator.h
+++ src/mutator.h
@@ -3,7 +3,7 @@
 
 
 /*
- Combining samplers for location and for mutation type into a mutation sampler for
+ Combining samplers for substitutions and indels into a mutation sampler for
  a single chromosome.
  */
 
@@ -19,8 +19,9 @@
 #include "var_classes.h"  // Var* classes
 #include "pcg.h"  // pcg seeding
 #include "alias_sampler.h"  // alias method of sampling
-#include "mutator_location.h"  // mutator location sampling classes
-#include "mutator_type.h"      // mutator type sampling classes
+#include "mutator_subs.h"   // SubMutator
+#include "mutator_indels.h" // IndelMutator
+#include "io.h"  // FileUncomp
 #include "util.h"  // str_stop
 
 
@@ -37,15 +38,6 @@ using namespace Rcpp;
  */
 class MutationSampler {
 
-    /*
-     Create a new string of nucleotides (for insertions) of a given length and using
-     an input rng engine
-     */
-    inline std::string new_nucleos(const uint64& len, pcg64& eng) const {
-        std::string str(len, 'x');
-        insert.sample(str, eng);
-        return str;
-    }
     // Does most of the work for the mutate methods (all but location sampling)
     inline double mutate__(pcg64& eng, const uint64& start, sint64& end);
 
@@ -53,44 +45,37 @@ public:
 
     // VarChrom object pointer to be manipulated
     VarChrom* var_chrom;
-    // For sampling the mutation location:
-    LocationSampler location;
-    // For sampling the type of mutation:
-    MutationTypeSampler type;
-    // For new insertion chromosomes:
-    AliasStringSampler<std::string> insert;
+    // For adding substitutions:
+    SubMutator subs;
+    // For adding indels:
+    IndelMutator indels;
+
 
     MutationSampler() : var_chrom(nullptr) {}
 
     MutationSampler(const MutationSampler& other)
-        : var_chrom(other.var_chrom), location(other.location), type(other.type),
-          insert(other.insert) {}
+        : var_chrom(other.var_chrom), subs(other.subs), indels(other.indels) {}
 
     MutationSampler& operator=(const MutationSampler& other) {
         if (other.var_chrom) var_chrom = other.var_chrom;
-        location = other.location;
-        type = other.type;
-        insert = other.insert;
+        subs = other.subs;
+        indels = other.indels;
         return *this;
     }
 
-    void new_chrom(VarChrom& vs_, const arma::mat& gamma_mat) {
-        var_chrom = &vs_;
-        location.new_chrom(vs_, gamma_mat);
+    void new_chrom(VarChrom& var_chrom_, pcg64& eng) {
+        var_chrom = &var_chrom_;
+        subs.new_chrom(var_chrom_, eng);
+        indels.new_chrom(var_chrom_);
         return;
     }
 
-    // Add mutation and return the change in the chromosome rate that results
-    double mutate(pcg64& eng);
-
     /*
-     Overloaded for only mutating within a range.
-     It also updates `end` if an indel occurs in the range.
-     Make sure to keep checking for situation where `end < start` (i.e., chromosome section
-     is empty).
-     `// ***` mark difference between this and previous `mutate` versions
+     Add mutations for a branch within a range:
+     It also updates `end` for indels that occur in the range.
+     Returns true only if the chromosome section is empty.
      */
-    double mutate(pcg64& eng, const uint64& start, sint64& end);
+    bool mutate(const double& b_len, pcg64& eng, const uint64& start, uint64& end);
 
 };
 
